7b6f035c288c4db0d1a5c0de4e599ce5c636e006
Felipe Santos <tfelipesantos99s@gmail.com>
2022-03-18 17:16:08 -0300

    Create transaction table component

--- src/components/TransactionTable/index.tsx
+++ src/components/TransactionTable/index.tsx
@@ -1,6 +1,13 @@
+import { useEffect } from "react";
 import { Container } from "./styles";
 
-export function TransactionTable() {
+export function TransactionsTable() {
+  useEffect(() => {
+    fetch("http://localhost:3000/api/transitions")
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  }, []);
+
   return (
     <Container>
       <table>
